92b62337891be6c9318bcae3e5770cce3aa2b51c
sazidhussain-tech <sazidhussain.tech@gmail.com>
2026-07-18 06:25:38 +0530

    Update database connection for deployment

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "u0_a451",
-    host: "localhost",
-    database: "hospital_db",
-    password: "",
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
